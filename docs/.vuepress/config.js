93df9cf3b41d9ff7ad0e9a9a37888a7dee4b558f
yemuysy <1458187661@qq.com>
2019-10-28 23:55:24 +0800

    成功的第一步

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,35 @@
 module.exports = {
     title: '码农——zxh',
     description: 'Just playing around',
+    locales: {
+      '/': {
+        lang: 'zh-CN'
+      }
+    },
+    head: [
+      ['link', { rel:'icon', href: '/favicon.png' } ]
+    ],
+    base: '/documents/',
     themeConfig: {
         lastUpdated: '上次更新时间',
+        repo: 'yemuysy/documents',
+        repoLabel: 'GitHub',
+        editLinks: true,
+        editLinkText: 'Edit this page',
+        // 以下为可选的编辑链接选项
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'yemuysy/documents',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'master',
         nav: [
           { text: 'Home', link: '/' },
-          { text: '插本', link: '/guide/' },
+          { text: '插本', link: '/' },
           { text: '在线工具', 
             items: [
-                { text: '展示1', link: 'https://google.com' },
-                { text: '展示2', link: 'https://google.com' }
+                { text: '展示1', link: 'https:baidu.com' },
+                { text: '展示2', link: 'https:baidu.com' }
             ] 
           }
         ],
@@ -21,17 +41,14 @@ module.exports = {
             ],
       
             '/about2/': [
-              '',      /* /bar/ */
+              '',      /* /about2/ */
             ],
       
             // fallback
             '/': [
-              '',        /* / */
-              'contact', /* /contact.html */
-              'about'    /* /about.html */
+              '',
             ]
         },
-        sidebarDepth: 2,
-        
+        sidebarDepth: 2
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,6 @@
 ---
 home: true
-# heroImage: img/hero.jpg
+heroImage: /avatar.jpg
 heroText: Hero 标题
 tagline: Hero 副标题
 actionText: 快速上手 →
